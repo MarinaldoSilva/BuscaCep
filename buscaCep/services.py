7f79b39902accfedd5319e0a578c109b4071f436
Mario <marinaldo12@hotmail.com>
2025-09-12 11:34:31 -0300

    service de  busca cep criada

---
+++ buscaCep/services.py
@@ -0,0 +1,33 @@
+import requests
+#com o request podemos enviar solicitações para web
+
+
+class ViaCEPService:
+
+    """
+    com esse decorator não precisamos instanciar a classe para usar a função.
+    Podemos chamar a partir da classe
+    """
+    @staticmethod
+    def get_cep(cep: str):
+        """
+        vamos receber o cep e passar a informação para url
+        e nossa resposta vai ser o retorno da request da consulta da url
+        se tiver algum erro a raise_for_status() vai lançar uma exception
+        """
+        url = f"https://viacep.com.br/ws/{cep}/json/"
+        
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            dados_cep = response
+
+            if "erro" in dados_cep:
+                return f"Informações não encontradas."
+            return dados_cep
+        
+        except requests.exceptions:
+            return f"Não foi possível conectar, tenho noavemnte mais tarde"
+
+        
+
