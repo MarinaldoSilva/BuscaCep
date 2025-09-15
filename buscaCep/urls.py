f6e81ba9492ca2ec18f7d8bd3a2e7116422a39b8
Mario <marinaldo12@hotmail.com>
2025-09-14 23:10:06 -0300

    correção das urls

--- buscaCep/urls.py
+++ buscaCep/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from Views.views_old import consultar_cep
+from .Views.views_old import consultar_cep
 
 urlpatterns = [
     path("cep/<str:cep>", consultar_cep, name="consultar-cep")
