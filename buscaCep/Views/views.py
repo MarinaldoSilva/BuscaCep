29f3f8e026402d129af0dc7392b3cc08fb1617e3
Mario <marinaldo12@hotmail.com>
2025-09-18 17:00:37 -0300

    adicioção do viewset para modernizar a forma de consulta a api

---
+++ buscaCep/Views/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from serializers import EnderecoSerializer
+from models import Endereco
+from services import ViaCEPService
+
+
+class EnderecoViewSet(viewsets.ModelViewSet):
+    serializer_class = EnderecoSerializer
+    queryset = Endereco.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        cep_param = request.data.get('cep')
+
+        if not cep_param:
+            return Response(
+                {"error": "O cep é obrigatório."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        dados_cep = ViaCEPService.get_cep(cep_param)
+
+        if isinstance(dados_cep, str):
+            return Response(
+                {"error": dados_cep},status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer = self.get_serializer(data=dados_cep)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
