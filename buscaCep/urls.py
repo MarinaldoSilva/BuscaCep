12feacab5f32fd1927f9f9f4f0602aac27c084f8
Mario <marinaldo12@hotmail.com>
2025-09-20 15:59:46 -0300

    configuração de rotas para suportar o viewSet

--- buscaCep/urls.py
+++ buscaCep/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .Views.views_old import consultar_cep
+from .Views.views import EnderecoViewSet
+
+router = DefaultRouter()
+
+router.register(r'consultar_cep', EnderecoViewSet)
 
 urlpatterns = [
-    path("cep/<str:cep>", consultar_cep, name="consultar-cep")
+    path("cep/<str:cep>", consultar_cep, name="consultar-cep"),
+
+    path('', include(router.urls))
 ]
